65fda8263aa47b94c08a6ca9f7e6e25f871560fb
zainokta <zainokta@gmail.com>
2018-12-24 15:43:22 +0700

    Episode 10 : Form and CSRF

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -11,6 +11,21 @@ class ProjectsController extends Controller
     public function index(){
         $projects = Project::all();
 
-        return view ('projects.index',compact('projects'));
+        return view ('projects.index', compact('projects'));
+    }
+
+    public function create(){
+        return view('projects.create');
+    }
+
+    public function store(){
+        $project = new Project();
+
+        $project->title = request('title');
+        $project->description = request('description');
+
+        $project->save();
+
+        return redirect('/projects');
     }
 }
---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+</head>
+
+<body>
+    <h1>Create a New Projects</h1>
+
+    <form method="POST" action="/projects">
+        {{ csrf_field() }}
+        <div>
+            <input type="text" name="title" placeholder="Project Title">
+        </div>
+        <div>
+            <textarea name="description" placeholder="Project Description"></textarea>
+        </div>
+        <div>
+            <button type="submit">Create Project</button>
+        </div>
+    </form>
+</body>
+
+</html>
