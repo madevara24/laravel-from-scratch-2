b4ffe3aaec81d40e53d89f5a6aa3116d3cb5a574
zainokta <zainokta@gmail.com>
2018-12-25 19:48:49 +0700

    Eps 16

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,8 +1,26 @@
 @extends('layout')
-@section('content')
-<h1 class="title">{{$project->title}}</h1>
-<div class="content">{{$project->description}}</div>
-<p>
-    <a href="/projects/{{$project->id}}/edit">Edit</a>
-</p>
+    @section('content')
+        <h1 class="title">{{$project->title}}</h1>
+        <div class="content">
+            {{$project->description}}
+            <p>
+                <a href="/projects/{{$project->id}}/edit">Edit</a>
+            </p>
+        </div>
+
+        @if ($project->tasks->count())
+            <div>
+                @foreach ($project->tasks as $task)
+                    <div>
+                        <form method="POST" action="/tasks/{{ $task->id }}">
+                            @method('PATCH')
+                            @csrf
+                            <label class="checkbox" for="completed">
+                                <input type="checkbox" name="completed" onchange="">
+                        </form>
+                        {{ $task->description }}
+                    </div>
+                @endforeach
+            </div>
+        @endif
 @endsection
